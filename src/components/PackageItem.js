6ba719be072065791c8037e2ffd41c3fbcc9903f
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-07-18 14:16:56 +0200

    Rewrite the PackageItem component to a proper class, as this makes it easier to ex. fetch the element if it is missing.

--- src/components/PackageItem.js
+++ src/components/PackageItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 var FontAwesome = require('react-fontawesome')
 
@@ -6,25 +6,23 @@ const progress = (
   <div><FontAwesome name='circle-o-notch' spin /> Loading data...</div>
 )
 
-const Component = ({ inprogress, item, actClosePackage }) => (
-  <div className="container">
-    <div className="uninett-color-white uninett-padded gutter">
-      {inprogress ? progress : null }
-      <button onClick={actClosePackage} type="button" className="close">
-        <span aria-hidden="true">×</span>
-        <span className="sr-only">Close alert</span>
-      </button>
-      <h2>{item.newest_chart.Name}</h2>
-      <h4>{item.newest_chart.Version}</h4>
-      <h6>{item.repo}</h6>
+class PackageItem extends Component {
+  render() {
+    return (
+    <div className="container">
+      <div className="uninett-color-white uninett-padded gutter">
+        {this.props.inprogress ? progress : null }
+        <button onClick={this.props.actClosePackage} type="button" className="close">
+          <span aria-hidden="true">×</span>
+          <span className="sr-only">Close alert</span>
+        </button>
+        <h2>{this.props.item.newest_chart.Name}</h2>
+        <h4>{this.props.item.newest_chart.Version}</h4>
+        <h6>{this.props.item.repo}</h6>
+      </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
-// Component.propTypes = {
-//   onClick: PropTypes.func.isRequired,
-//   completed: PropTypes.bool.isRequired,
-//   text: PropTypes.string.isRequired
-// }
-
-export default Component
+export default PackageItem
